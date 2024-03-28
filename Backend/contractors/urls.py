c66b85f75382fbcc21f1da404c1efd90202baa90
DeVante Parker <108905616+TechTay@users.noreply.github.com>
2024-03-28 17:45:44 -0500

    Delete Backend directory
    adding anew

--- Backend/contractors/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path, include
-from contractors import views
-
-
-urlpatterns = [
-    path('', views.user_contractor),
-    path('all/', views.get_all_contractors),
-]
--- Backend/projects/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import Project
-# Register your models here.
-admin.site.register(Project)
